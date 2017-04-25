1d7697d014fede7e8280131a5f5362468e1fc75d
MacMan <x.client@yahoo.com>
2017-04-25 22:32:18 +0530

    Adds Solo model, implemented in index view

--- jmad/solos/views.py
+++ jmad/solos/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+from .models import Solo
 
 # Create your views here.
 def index(request):
-    return render_to_response('solos/index.html')
+    context = {'solos' : Solo.objects.filter(
+        instrument=request.GET.get(
+            'instrument', None
+            )
+        )}
+    return render_to_response('solos/index.html', context)
