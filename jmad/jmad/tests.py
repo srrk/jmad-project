338fcfc6a7abc57cfab93d0c7f41d27b3856a8e4
SR Rajesh Kumar <rajeshux@gmail.com>
2017-04-26 14:41:46 +0530

    Adds a tag to search results, small test refactor

--- jmad/jmad/tests.py
+++ jmad/jmad/tests.py
@@ -28,6 +28,11 @@ class StudentTestCase(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def find_search_results(self):
+        return self.browser.find_elements_by_css_selector(
+                '.jmad-search-result a'
+            )
+
     def test_student_find_solos(self):
         """
         Test that a user can search for solos
@@ -65,9 +70,8 @@ class StudentTestCase(LiveServerTestCase):
                 .find_element_by_css_selector('form button').click()
 
         # He sees too many search results...
-        search_results = self.browser.find_elements_by_css_selector(
-                        '.jmad-search-result'
-                        )
+        search_results = self.find_search_results()
+                        
         self.assertGreater(len(search_results), 2)
 
         # ...so he adds an artist to his search query and 
@@ -77,8 +81,7 @@ class StudentTestCase(LiveServerTestCase):
         second_artist_input.send_keys('John Coltrane')
         self.browser\
             .find_element_by_css_selector('form button').click()
-        second_search_results = self.browser\
-            .find_elements_by_css_selector('.jmad-search-result')
+        second_search_results = self.find_search_results()
         self.assertEqual(len(second_search_results), 1)
 
         # He clicks on a search result.
