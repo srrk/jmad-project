5a2d12c5291772ae85f4ce562bdca69e0750baf9
SR Rajesh Kumar <rajeshux@gmail.com>
2017-04-27 18:03:44 +0530

    Staff login, initial model admin registration

--- jmad/jmad/tests.py
+++ jmad/jmad/tests.py
@@ -1,4 +1,5 @@
 from django.test import LiveServerTestCase
+from django.contrib.auth import get_user_model
 from selenium import webdriver
 
 from solos.models import Solo
@@ -21,12 +22,52 @@ class StudentTestCase(LiveServerTestCase):
         # title of the page
         self.assertEqual(self.browser.title,
                 'Log in | Django site admin')
-        self.fail('Incomplete Test')
 
         # He enters his username and password and submits the
         # form to log in
+        login_form = self.browser.find_element_by_id(
+                'login-form')
+        login_form.find_element_by_name('username').\
+                send_keys('bill')
+
+        login_form.find_element_by_name('password').\
+                send_keys('password')
+        login_form.find_element_by_css_selector(
+                '.submit-row input').click()
 
         # He sees links to Albums, Tracks and Solos
+        albums_links = self.browser.\
+                find_elements_by_link_text('Albums')
+
+        self.assertEqual(
+                albums_links[0].get_attribute('href'),
+                self.live_server_url + '/admin/albums/'
+        )
+
+        self.assertEqual(
+                albums_links[1].get_attribute('href'),
+                self.live_server_url + '/admin/albums/album/'
+        )
+
+        self.assertEqual(
+                self.browser.\
+                        find_element_by_link_text('Tracks').\
+                        get_attribute('href'),
+                self.live_server_url + '/admin/albums/track/'
+        )
+
+        solos_links = self.browser.\
+                find_elements_by_link_text('Solos')
+
+        self.assertEqual(
+                solos_links[0].get_attribute('href'),
+                self.live_server_url + '/admin/solos/'
+        )
+        self.assertEqual(
+                solos_links[1].get_attribute('href'),
+                self.live_server_url + '/admin/solos/solo/'
+        )
+        self.fail('Incomplete Test')
 
         # He clicks on Albums and sees all of the Albums that
         # have been added so far
@@ -69,9 +110,15 @@ class StudentTestCase(LiveServerTestCase):
         # solo.
 
     def setUp(self):
-        # self.browser = webdriver.Firefox()
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(2)
+
+        self.admin_user = get_user_model().objects.create_superuser(
+                    username='bill',
+                    email='bill@example.com',
+                    password='password'
+           )
+
         self.album1 = Album.objects.create(
                 name='My Favorite Things',
                 slug='my-favorite-things'
