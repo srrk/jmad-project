079cee45bd78122db0b23229382523be3c922a1f
MacMan <x.client@yahoo.com>
2017-04-27 08:06:31 +0530

    New app albums added

--- jmad/solos/models.py
+++ jmad/solos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Solo(models.Model):
@@ -11,3 +12,9 @@ class Solo(models.Model):
     end_time = models.CharField(max_length=20, blank=True,
             null=True)
 
+    def get_absolute_url(self):
+        return reverse('solo_detail_view', kwargs={
+            'album' : self.track.album.slug,
+            'track' : self.track.slug,
+            'artist' : self.slug
+        })
--- jmad/solos/tests/test_models.py
+++ jmad/solos/tests/test_models.py
@@ -1,11 +1,26 @@
 from django.test import TestCase
 from solos.models import Solo
+from albums.models import Album, Track
 
 class SoloModelTestCase(TestCase):
     
     def setUp(self):
+
+        self.album = Album.objects.create(
+            name='At the Stratford Shakespearean Festival',
+            artist='Oscar Peterson Trio',
+            slug='at-the-stratford-shakespearean-festival'
+        )
+
+        self.track = Track.objects.create(
+            name='Falling in Love with Love',
+            album=self.album,
+            track_number=1,
+            slug='falling-in-love-with-love'
+        )
+
         self.solo = Solo.objects.create(
-            track='Falling in Love with Love',
+            track=self.track,
             artist='Oscar Peterson',
             instrument='piano',
             album='At the Stratford Shakespearean Festival',
@@ -19,3 +34,13 @@ class SoloModelTestCase(TestCase):
         '''
         self.assertEqual(self.solo.artist, 'Oscar Peterson')
         self.assertEqual(self.solo.end_time, '4:06')
+
+    def test_get_absolute_url(self):
+        '''
+        Test that we can build a URL for a solo
+        '''
+        self.assertEqual(
+            self.solo.get_absolute_url(),
+            '/recordings/at-the-stratford-shakespearean-festival/'
+            'falling-in-love-with-love/oscar-peterson/'
+        )
